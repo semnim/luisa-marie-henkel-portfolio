e8c8e0d4d51c91ac01daf1edf540c7b6df326001
Semjon Nirmann <82184629+semnim@users.noreply.github.com>
2025-11-03 17:24:18 +0100

    Improve mobile navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 import { DesktopNavMenu } from '@/components/desktop-nav-menu';
 import './globals.css';
 
-import { MobileDrawer } from '@/components/mobile-drawer';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Inter } from 'next/font/google';
 
@@ -18,9 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased flex flex-col`}>
-        <header className="absolute inset-x-0 z-50 text-muted-foreground items-center flex justify-end px-8 py-4 bg-linear-to-b from-black/75 to-transparent h-25">
-          <MobileDrawer triggerClassName="md:hidden" />
-          <DesktopNavMenu className="hidden md:flex" />
+        <header className="bottom-0 md:top-0 absolute inset-x-0 z-50 text-muted-foreground items-center flex justify-end px-8 md:px-8 py-4 bg-linear-to-t md:bg-linear-to-b from-black/75 to-transparent h-10 md:h-25">
+          <DesktopNavMenu className="flex" />
         </header>
         <ThemeProvider
           attribute="class"
--- components/section.tsx
+++ components/section.tsx
@@ -7,7 +7,7 @@ export const Section = ({
 }) => {
   const variants = {
     DEFAULT:
-      'h-dvh max-h-dvh md:h-screen md:max-h-screen pt-16 pb-8 lg:pb-16 md:pt-24 lg:pt-32 flex flex-col snap-center',
+      'h-dvh max-h-dvh md:h-screen md:max-h-screen pt-8 pb-8 lg:pb-16 md:pt-24 lg:pt-32 flex flex-col snap-center',
     HERO: 'h-dvh md:h-screen relative overflow-hidden snap-start',
   };
   return <section className={variants[variant]}>{children}</section>;
