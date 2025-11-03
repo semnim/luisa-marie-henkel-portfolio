8a9e09589133f6186148740d561aa9bd4e92ea36
Semjon Nirmann <82184629+semnim@users.noreply.github.com>
2025-11-03 14:36:46 +0100

    Refine contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,8 @@
 import { BackgroundVideo } from '@/components/background-video';
+import { ContactInfoList } from '@/components/contact/contact-info-list';
 import { Container } from '@/components/container';
 import { Heading } from '@/components/heading';
 import { Section } from '@/components/section';
-import { Separator } from '@/components/ui/separator';
-import { Instagram, Mail, Phone } from 'lucide-react';
-import Link from 'next/link';
 
 export const metadata = {
   title: 'Contact | Luisa-Marie Henkel',
@@ -18,51 +16,11 @@ export default function ContactPage() {
         <BackgroundVideo url="/assets/hero_bg_falls_2.mp4">
           <Heading
             title="REACH OUT"
-            context={<ContactInfo />}
+            context={<ContactInfoList />}
             containerClassName="h-full"
           />
         </BackgroundVideo>
       </Section>
     </Container>
-  );
-}
-
-function ContactInfo() {
-  return (
-    <ul className="text-lg tracking-[0.25rem] flex flex-col md:flex-row gap-4 font-light">
-      <li>
-        <Link
-          className="flex gap-2 items-center justify-center text-muted-foreground hover:text-foreground"
-          href="tel:+49 15126113117"
-        >
-          <Phone size={15} />
-          <span>+49 15126113117</span>
-        </Link>
-      </li>
-      <div className="hidden md:block">
-        <Separator orientation="vertical" />
-      </div>
-      <li>
-        <Link
-          className="flex gap-2 items-center justify-center text-muted-foreground hover:text-foreground"
-          href="mailto:luisamariehenkel@gmail.com"
-        >
-          <Mail size={15} />
-          <span>luisamariehenkel@gmail.com</span>
-        </Link>
-      </li>
-      <div className="hidden md:block">
-        <Separator orientation="vertical" />
-      </div>
-      <li>
-        <Link
-          className="flex gap-2 items-center justify-center text-muted-foreground hover:text-foreground"
-          href="https://www.instagram.com/oh_luisa/"
-        >
-          <Instagram size={15} />
-          <span>@oh_luisa</span>
-        </Link>
-      </li>
-    </ul>
   );
 }
---
+++ components/contact/contact-info-list.tsx
@@ -0,0 +1,134 @@
+'use client';
+
+import { PopoverArrow, PopoverContent } from '@radix-ui/react-popover';
+import {
+  Check,
+  CopyIcon,
+  Instagram,
+  LucideIcon,
+  Mail,
+  Phone,
+} from 'lucide-react';
+import Link from 'next/link';
+import { ComponentProps, useCallback, useState } from 'react';
+import { Button } from '../ui/button';
+import { Popover, PopoverTrigger } from '../ui/popover';
+
+const SOCIAL_ITEMS = ['tel', 'mail', 'instagram'] as const;
+type SocialItem = (typeof SOCIAL_ITEMS)[number];
+
+const ICONS = {
+  tel: Phone,
+  mail: Mail,
+  instagram: Instagram,
+};
+const SOCIALS = {
+  tel: { content: '+49 15126113117', uri: 'tel:+4915126113117' },
+  mail: {
+    content: 'luisamariehenkel@gmail.com',
+    uri: 'mailto:luisamariehenkel@gmail.com',
+  },
+  instagram: {
+    content: '@oh_luisa',
+    uri: 'https://www.instagram.com/oh_luisa/',
+  },
+};
+
+export const ContactInfoList = () => {
+  const [isCopied, setIsCopied] = useState<Record<SocialItem, boolean>>({
+    mail: false,
+    tel: false,
+    instagram: false,
+  });
+
+  const handleIsCopied = (item: SocialItem, isCopied: boolean) => {
+    setIsCopied((prev) => ({ ...prev, [item]: isCopied }));
+  };
+  const handleCopy = useCallback(
+    (item: SocialItem) => {
+      if (isCopied[item]) return;
+
+      const content = SOCIALS[item].content;
+      navigator.clipboard
+        .writeText(content)
+        .then(() => {
+          handleIsCopied(item, true);
+          setTimeout(() => handleIsCopied(item, false), 3000);
+        })
+        .catch((error) => {
+          console.error('Error copying command', error);
+        });
+    },
+    [isCopied]
+  );
+
+  return (
+    <>
+      <div className="text-lg tracking-[0.25rem] flex items-start pt-4 gap-4 font-light">
+        {SOCIAL_ITEMS.map((item) => {
+          const Icon = ICONS[item];
+          return (
+            <Popover key={item}>
+              <PopoverTrigger asChild>
+                <Button
+                  size="icon"
+                  variant={'ghost'}
+                  className="cursor-pointer"
+                >
+                  <Icon size={25} className="text-foreground cursor-pointer" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                side="bottom"
+                className="flex bg-white/20 rounded-sm pl-4 gap-1 pr-2 py-2 items-center tracking-widest"
+              >
+                <PopoverArrow className=" fill-white/20" />
+                <SocialsPopoverInfo
+                  item={item}
+                  isCopied={isCopied[item]}
+                  handleCopy={handleCopy}
+                />
+              </PopoverContent>
+            </Popover>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+function CopySocialIcon({
+  isCopied,
+  ...props
+}: { isCopied: boolean } & ComponentProps<LucideIcon>) {
+  if (isCopied) {
+    return <Check {...props} />;
+  }
+
+  return <CopyIcon {...props} />;
+}
+
+function SocialsPopoverInfo({
+  item,
+  isCopied,
+  handleCopy,
+}: {
+  item: SocialItem;
+  isCopied: boolean;
+  handleCopy: (item: SocialItem) => void;
+}) {
+  return (
+    <>
+      <Link
+        href={SOCIALS[item].uri}
+        className="underline text-sm"
+        target="_blank"
+      >
+        {SOCIALS[item].content}
+      </Link>
+      <Button variant={'ghost'} onClick={() => handleCopy(item)}>
+        <CopySocialIcon isCopied={isCopied} className="w-3! h-3!" />
+      </Button>
+    </>
+  );
+}
