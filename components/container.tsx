dcfaead5e5e9d52e7feb864c217564979a503d7f
Semjon Nirmann <82184629+semnim@users.noreply.github.com>
2025-11-04 17:03:18 +0100

    Fix double scroll bar in work page

--- components/container.tsx
+++ components/container.tsx
@@ -1,15 +1,17 @@
 export const Container = ({
   children,
   disableScrollSnap = false,
+  disableScroll = false,
 }: {
+  disableScroll?: boolean;
   disableScrollSnap?: boolean;
   children: React.ReactNode;
 }) => {
   return (
     <main
-      className={`h-dvh md:h-screen overflow-y-scroll ${
-        !disableScrollSnap ? 'snap-y snap-mandatory' : ''
-      }`}
+      className={`h-dvh md:h-screen ${
+        !disableScroll ? 'overflow-y-scroll' : ''
+      } ${!disableScrollSnap ? 'snap-y snap-mandatory' : ''}`}
     >
       {children}
     </main>
