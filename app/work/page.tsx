e877bfbba3eddb1449d2e1805cb451bd343d7857
Semjon Nirmann <82184629+semnim@users.noreply.github.com>
2025-11-04 10:33:49 +0100

    Experimental scroll snap on mobile

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,13 +1,15 @@
+'use client';
 import { Container } from '@/components/container';
 import { Heading } from '@/components/heading';
 import { Section } from '@/components/section';
+import { useIsMobile } from '@/hooks/use-is-mobile';
 import { createSlugForWorkItem } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
-export const metadata = {
-  title: 'Work | Luisa-Marie Henkel',
-  description: 'Art director & Stylist portfolio',
-};
+// export const metadata = {
+//   title: 'Work | Luisa-Marie Henkel',
+//   description: 'Art director & Stylist portfolio',
+// };
 
 const PROJECTS = [
   {
@@ -18,13 +20,13 @@ const PROJECTS = [
   { id: 2, title: 'Tangled Manners', thumbnailUrl: '/placeholders/2/1.webp' },
   { id: 3, title: 'Madame Rêve', thumbnailUrl: '/placeholders/3/1.webp' },
   { id: 4, title: 'Brothers', thumbnailUrl: '/placeholders/4/1.webp' },
-  null,
-  null,
-  null,
+  // null,
+  // null,
+  // null,
   { id: 5, title: 'Fading Ties', thumbnailUrl: '/placeholders/4/2.webp' },
-  null,
-  null,
-  null,
+  // null,
+  // null,
+  // null,
   {
     id: 6,
     title: 'Pioneer to the falls',
@@ -35,25 +37,27 @@ const PROJECTS = [
     title: 'Les Nocés Funebres',
     thumbnailUrl: '/placeholders/4/4.webp',
   },
-  null,
-  null,
+  // null,
+  // null,
 ];
 export default function WorkPage() {
   // container heading: bg-transparent absolute top-0 top-0 left-0 right-0 lg:pt-20 pt-8
+  const isMobile = useIsMobile();
+
   return (
     <Container disableScrollSnap>
-      <Section className="overflow-y-scroll max-h-dvh snap-y snap-proximity pt-0 lg:pt-32">
-        <Heading title="PROJECTS" containerClassName="fixed top-20 inset-x-0" />
+      <Heading title="PROJECTS" containerClassName="fixed top-20 inset-x-0" />
+      <Section className="overflow-y-scroll max-h-dvh snap-y snap-mandatory pt-0 lg:pt-32">
         <div className="grid grid-cols-1 lg:grid-cols-3 relative">
           {PROJECTS.map((item, index) => {
             if (item === null) {
-              return <div key={'empty' + index} />;
+              return <div className="h-0" key={'empty' + index} />;
             }
             return (
               <Link
                 href={`/work/${createSlugForWorkItem(item.title)}`}
                 key={item.id}
-                className={` group overflow-hidden sticky top-0 snap-start h-dvh lg:h-[calc(100dvh-192px)]`}
+                className={` group overflow-hidden snap-center relative h-dvh lg:h-[500px]`}
               >
                 <figure
                   key={item.id}
